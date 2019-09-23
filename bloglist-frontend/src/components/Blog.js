63d79956aeeb7cbc0adba319cbcb112cb9c07d0c
Tuomas Alanen <tuomas@paaryna.fi>
2019-09-23 20:15:13 +0300

    5.6

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+
+
+const Blog = (props) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'dotted',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+
+
+  const showWhenVisible = { display: visible ? '' : 'none'}
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => toggleVisibility()}>
+        {props.blog.title} {props.blog.author}
+        <div style={showWhenVisible}>
+          <div>{props.blog.url}</div>
+          <div>{props.blog.likes} likes <button>like</button></div>
+          <div>added by {props.blog.user.name}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
